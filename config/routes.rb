c17861031fd5873e20c33de47b42d1cba6564a33
railshive <ewilkins@railshive.com>
2023-04-03 15:16:56 -0500

    added request to ChatGPT

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  post 'ai_request', to: 'pages#ai_request'
+  root 'pages#home'
 end
